9a2bc59d2d6e5b3c969711b305563f18ff165a0b
Bárbara <barbara.sohne@duque.g12.br>
2025-08-25 19:40:30 -0300

    Primeiro commit da aplicação

---
+++ Aula04/src/main/java/com/barbara/aula04/Aula04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.barbara.aula04;
+
+/**
+ *
+ * @author 08315
+ */
+public class Aula04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
