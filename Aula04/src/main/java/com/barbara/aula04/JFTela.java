263006ad1a6bcce390b5a0a883bf02d0643704ec
Bárbara <barbara.sohne@duque.g12.br>
2025-08-25 21:43:51 -0300

    Vou repetir x vezes

--- Aula04/src/main/java/com/barbara/aula04/JFTela.java
+++ Aula04/src/main/java/com/barbara/aula04/JFTela.java
@@ -39,6 +39,8 @@ public class JFTela extends javax.swing.JFrame {
         txtAtraso = new javax.swing.JTextField();
         lbTotal = new javax.swing.JLabel();
         btnCalcular = new javax.swing.JButton();
+        btnIf = new javax.swing.JButton();
+        btnLoop = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -79,6 +81,20 @@ public class JFTela extends javax.swing.JFrame {
             }
         });
 
+        btnIf.setText("If....else");
+        btnIf.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnIfActionPerformed(evt);
+            }
+        });
+
+        btnLoop.setText("Loop");
+        btnLoop.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLoopActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -99,7 +115,9 @@ public class JFTela extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(btnContar)
-                            .addComponent(btnCalcular)))
+                            .addComponent(btnCalcular)
+                            .addComponent(btnIf)
+                            .addComponent(btnLoop)))
                     .addComponent(lbTotal))
                 .addContainerGap(12, Short.MAX_VALUE))
         );
@@ -121,14 +139,18 @@ public class JFTela extends javax.swing.JFrame {
                 .addGap(10, 10, 10)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtJuros, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(txtJuros, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnIf))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtAtraso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtAtraso, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnLoop))
                 .addGap(18, 18, 18)
                 .addComponent(lbTotal)
-                .addContainerGap(45, Short.MAX_VALUE))
+                .addContainerGap(43, Short.MAX_VALUE))
         );
 
         pack();
@@ -160,6 +182,32 @@ public class JFTela extends javax.swing.JFrame {
         double montante = valor + juros;
         lbTotal.setText("Total a pagar R$"+montante);
     }//GEN-LAST:event_btnCalcularActionPerformed
+
+    private void btnIfActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIfActionPerformed
+        // TODO add your handling code here:
+        int a = Integer.valueOf(txtPagamento.getText());
+        int b = Integer.valueOf(txtJuros.getText());
+        int c = Integer.valueOf(txtAtraso.getText());
+        String mensagem = "";
+        if(a>b && a>c) {
+            mensagem = "A é o maior valor";
+        }else if(b>a && b>c) {
+            mensagem = "B é o maior valor";
+        }else if(c>a && c>b) {
+            mensagem = "C é o maior valor";
+        }else {
+            mensagem = "Resultado não esperado";
+        }
+        JOptionPane.showMessageDialog(null, mensagem);
+        
+    }//GEN-LAST:event_btnIfActionPerformed
+
+    private void btnLoopActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoopActionPerformed
+        // TODO add your handling code here:
+        for(int i=1; i<=5; i++){
+            JOptionPane.showMessageDialog(null, "Vou repetir "+ i+" vezes");
+        }
+    }//GEN-LAST:event_btnLoopActionPerformed
 
     /**
      * @param args the command line arguments
@@ -200,6 +248,8 @@ public class JFTela extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCalcular;
     private javax.swing.JButton btnContar;
+    private javax.swing.JButton btnIf;
+    private javax.swing.JButton btnLoop;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
